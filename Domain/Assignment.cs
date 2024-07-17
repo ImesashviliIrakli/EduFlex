8f25e015ad78acbe82cc3d17523d3257f58f06eb
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-07-18 03:08:36 +0400

    Started adding the domain for Homework assignment logic

---
+++ Domain/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class Assignment
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+
+    public int TeacherCourseId { get; set; }
+    [Required]
+    public required string Title { get; set; }
+    [Required]
+    public required string Description { get; set; }
+    [Required]
+    public required string FileUrl { get; set; }
+    [Required]
+    public int MaxGrade { get; set; }
+    [Required]
+    public int MinGrade { get; set; }
+
+}
---
+++ Domain/Homework.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class Homework
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public int EnrollmentId { get; set; }
+    [Required]
+    public int AssignmentId { get; set; }
+    [Required]
+    public int StudentId { get; set; }
+    [Required]
+    public required string FileUrl { get; set; }
+    [Required]
+    public int Grade { get; set; }
+}
