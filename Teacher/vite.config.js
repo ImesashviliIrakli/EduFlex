8a4da1d4be29ea114c41c1851728e1e01f20af16
NikaGogelia <gogelianika1@gmail.com>
2024-07-18 00:10:43 +0400

    created teacher admin

---
+++ Teacher/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3002,
+    },
+    // esbuild: {
+    //   loader: { ".js": "jsx" },
+    //   include: ["src/**/*.js", "src/**/*.jsx"],
+    //   exclude: [],
+    // },
+    // resolve: {
+    //   extensions: [".js", ".jsx"],
+    // },
+});
